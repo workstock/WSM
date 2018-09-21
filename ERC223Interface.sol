86f987fba9b44ebecb6f16d4792a7e44435c018a
workstock <41023403+workstock@users.noreply.github.com>
2018-09-21 19:37:28 +0300

    Add files via upload

---
+++ ERC223Interface.sol
@@ -0,0 +1,13 @@
+pragma solidity ^0.4.24;
+
+contract ERC223Interface {
+    function balanceOf(address who) constant returns (uint);
+    function transfer(address to, uint value) returns (bool);
+    function transfer(address to, uint value, bytes data) returns (bool);
+    event Transfer(address indexed from, address indexed to, uint256 value);
+    
+    function allowance(address owner, address spender) constant returns (uint256);
+    function transferFrom(address from, address to, uint256 value) returns (bool);
+    function approve(address spender, uint256 value) returns (bool);
+    event Approval(address indexed owner, address indexed spender, uint256 value);
+}
