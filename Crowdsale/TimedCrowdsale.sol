142955b8b785968a1fd3ae22234b5a99752e96d0
workstock <41023403+workstock@users.noreply.github.com>
2018-09-27 21:22:18 +0300

    Create TimedCrowdsale.sol

---
+++ Crowdsale/TimedCrowdsale.sol
@@ -0,0 +1,90 @@
+pragma solidity ^0.4.24;
+import "https://github.com/OpenZeppelin/openzeppelin-solidity/contracts/crowdsale/Crowdsale.sol"
+import "https://github.com/OpenZeppelin/openzeppelin-solidity/contracts/math/SafeMath.sol";
+
+/**
+ * @title TimedCrowdsale
+ * @dev Crowdsale accepting contributions only within a time frame.
+ */
+contract TimedCrowdsale is Crowdsale {
+  using SafeMath for uint256;
+
+  uint256 private _openingTime;
+  uint256 private _closingTime;
+  
+  uint32[] private _timePoints;
+  
+  function _checkArray(uint[] array) internal pure {
+        for (uint8 i = 0; i<array.length-1; i++){
+            require(array[i] < array[i+1]);
+        }
+    }
+
+  /**
+   * @dev Reverts if not in crowdsale time range.
+   */
+  modifier onlyWhileOpen {
+    require(isOpen());
+    _;
+  }
+
+  /**
+   * @dev Constructor, takes crowdsale opening and closing times.
+   * @param openingTime Crowdsale opening time
+   * @param closingTime Crowdsale closing time
+   */
+  constructor(uint32[] timePoints) public {
+    // solium-disable-next-line security/no-block-members
+    _checkArray(uint32[] timePoints)
+    _timePoints = timePoints;
+    _openingTime = _timePoints[0];
+    _closingTime = _timePoints[_timePoints.length-1];
+  }
+
+  /**
+   * @return the crowdsale opening time.
+   */
+  function openingTime() public view returns(uint256) {
+    return _openingTime;
+  }
+
+  /**
+   * @return the crowdsale closing time.
+   */
+  function closingTime() public view returns(uint256) {
+    return _closingTime;
+  }
+
+  /**
+   * @return true if the crowdsale is open, false otherwise.
+   */
+  function isOpen() public view returns (bool) {
+    // solium-disable-next-line security/no-block-members
+    return block.timestamp >= _openingTime && block.timestamp <= _closingTime;
+  }
+
+  /**
+   * @dev Checks whether the period in which the crowdsale is open has already elapsed.
+   * @return Whether crowdsale period has elapsed
+   */
+  function hasClosed() public view returns (bool) {
+    // solium-disable-next-line security/no-block-members
+    return block.timestamp > _closingTime;
+  }
+
+  /**
+   * @dev Extend parent behavior requiring to be within contributing period
+   * @param beneficiary Token purchaser
+   * @param weiAmount Amount of wei contributed
+   */
+  function _preValidatePurchase(
+    address beneficiary,
+    uint256 weiAmount
+  )
+    internal
+    onlyWhileOpen
+  {
+    super._preValidatePurchase(beneficiary, weiAmount);
+  }
+
+}
